c68e97cec566124900d9616b078edf7d0254b364
Kazuma Nara <nkz9528w.econ@gmail.com>
2021-12-29 22:53:01 +0900

    fix

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 export { RootCollection } from "./core/RootCollection";
-export { SubCollection, Collection } from "./core/SubCollection";
+export { SubCollection } from "./core/SubCollection";
 export { Reference } from "./core/Reference";
 export { collection } from "./core/Factory";
