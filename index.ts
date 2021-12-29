362b0e278b50867ebc1f9a981c1f13c6c0996cf1
Kazu <nkz9528w.econ@gmail.com>
2021-12-29 19:45:24 +0900

    add index

---
+++ index.ts
@@ -0,0 +1,4 @@
+export { RootCollection } from "./core/RootCollection";
+export { SubCollection, Collection } from "./core/SubCollection";
+export { Reference } from "./core/Reference";
+export { collection } from "./core/Factory";
